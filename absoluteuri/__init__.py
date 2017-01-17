ea3ffc83dbe0e8ff240eb026d93cfd5566b8a5f5
murchik <murchik@protonmail.com>
2017-01-17 17:49:00 +0800

    Ability to build absolute URI on any Site.

--- absoluteuri/__init__.py
+++ absoluteuri/__init__.py
@@ -2,14 +2,24 @@ import pkg_resources
 
 from django.core import urlresolvers
 from django.conf import settings
+from django.contrib.sites.models import Site
+
 
 __version__ = pkg_resources.get_distribution('django-absoluteuri').version
 
 
 def build_absolute_uri(path):
     """Turn a relative URL into an absolute URL."""
-    from django.contrib.sites.models import Site
-    site = Site.objects.get_current()
+    return build_absolute_uri_on_site(get_current_site(), path)
+
+
+def reverse(*args, **kwargs):
+    """Reverse a URL and make it absolute."""
+    return reverse_on_site(get_current_site(), *args, **kwargs)
+
+
+def build_absolute_uri_on_site(site, path):
+    """Turn a relative URL into an absolute URL on a certain site."""
     return '{protocol}://{domain}{path}'.format(
         protocol=getattr(settings, 'ABSOLUTEURI_PROTOCOL', 'http'),
         domain=site.domain,
@@ -17,7 +27,15 @@ def build_absolute_uri(path):
     )
 
 
-def reverse(*args, **kwargs):
-    """Reverse a URL and make it absolute."""
+def reverse_on_site(site, *args, **kwargs):
+    """Reverse a URL and make it absolute on a certain site."""
     path = urlresolvers.reverse(*args, **kwargs)
-    return build_absolute_uri(path)
+    return build_absolute_uri_on_site(site, path)
+
+
+def get_current_site():
+    return Site.objects.get_current()
+
+
+def get_site(site_pk):
+    return Site.objects.get(pk=site_pk)
